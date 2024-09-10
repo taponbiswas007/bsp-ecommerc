c4238ce1d127e85ca5718ff6d791fc1ce657ce80
taponbiswas007 <taponbiswassdp@gmail.com>
2024-09-10 17:59:24 +0600

    khkhjk

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
-$(document).ready(function(){
-    $(".dropdown").hover(function(){
-      $(this).find(".dropdown-menu").slideDown();
-    }, function(){
-      $(this).find(".dropdown-menu").slideUp();
-    });
-  
-    $(".cart-btn").on("click", function(){
-      $("#cartSidebar").toggleClass("active");
-    });
-  });
+// $(document).ready(function() {
+//   // Show submenu on hover
+//   $('.menu-item').hover(function () {
+//       // Hide other submenus
+//       $('.submenu').slideUp(300);
+//       // Show current submenu
+//       $(this).children('.submenu').stop().slideDown(300);
+//   }, function() {
+//       // Hide the submenu when not hovering over menu item
+//       $(this).children('.submenu').stop().slideUp(300);
+//   });
+
+//   // Hide submenu when clicking outside the menu
+//   $(document).click(function(event) {
+//       if (!$(event.target).closest('.nav-menu, .submenu').length) {
+//           $('.submenu').slideUp(300); // Hide all submenus
+//       }
+//   });
+// });
+
   
