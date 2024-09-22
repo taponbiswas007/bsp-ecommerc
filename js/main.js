b5e076003e1bf789287391b3b6fa1dd9fe3ab0e5
taponbiswas007 <taponbiswassdp@gmail.com>
2024-09-22 14:01:14 +0600

    slick add

--- js/main.js
+++ js/main.js
@@ -108,7 +108,45 @@
     //         $('.navbar').removeClass('sticky');
     //     }
     // });
-    
+
+
+    // slick slider
+
+    $('.category-slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2.5,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1.5,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
     
 })(jQuery);
 
